d92b0125d5d447d9ec152d404321e5f89f0b4b71
John Mburu <johnnkonge2020@gmail.com>
2023-09-19 11:13:03 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+function cName(){
+    const companyName ='Scruber';
+    console.log(companyName);
+}
+cName();
+
+function mostProf(){
+    let mostProfitableNeighborhood = 'Chelsea';
+    console.log(mostProfitableNeighborhood);
+}
+mostProf();
+
+function ceo(){
+    let CEO = 'Susan';
+    console.log(CEO);
+}
+ceo();
